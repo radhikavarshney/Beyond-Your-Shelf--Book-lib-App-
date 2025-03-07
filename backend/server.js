7e67a9863a12097a66a6087dd2fbf39891da570a
radhikavarshney <rvarshney2001@gmail.com>
2025-03-07 15:47:24 +0530

    updated urls

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 //middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://beyond-your-shelf.onrender.com", credentials: true })
+);
 app.use(express.json({ limit: "20mb" }));
 app.use(cookieParser());
 
--- frontend/src/store/authstore.js
+++ frontend/src/store/authstore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://beyond-your-shelf.onrender.com/api";
 axios.defaults.withCredentials = true;
 
 export const useAuthStore = create((set) => ({
